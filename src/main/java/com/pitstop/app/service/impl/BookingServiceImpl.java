1ad8d13c78f631d07bdb01f377bb587342223130
Zunaid Hossain <zunaidhossain.india@gmail.com>
2025-10-24 00:41:19 +0530

    Changes to implement Booking flow

--- src/main/java/com/pitstop/app/service/impl/BookingServiceImpl.java
+++ src/main/java/com/pitstop/app/service/impl/BookingServiceImpl.java
@@ -2,14 +2,23 @@ package com.pitstop.app.service.impl;
 
 import com.pitstop.app.constants.BookingStatus;
 import com.pitstop.app.constants.WorkshopStatus;
+import com.pitstop.app.dto.BookingRequestOtp;
+import com.pitstop.app.dto.BookingResponse;
+import com.pitstop.app.dto.BookingStatusResponse;
+import com.pitstop.app.dto.WorkshopStatusResponse;
 import com.pitstop.app.model.AppUser;
 import com.pitstop.app.model.Booking;
 import com.pitstop.app.model.WorkshopUser;
+import com.pitstop.app.repository.AppUserRepository;
 import com.pitstop.app.repository.BookingRepository;
+import com.pitstop.app.repository.WorkshopUserRepository;
 import com.pitstop.app.service.BookingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +29,9 @@ public class BookingServiceImpl implements BookingService {
     private final BookingRepository bookingRepository;
     private final AppUserServiceImpl appUserService;
     private final WorkshopUserServiceImpl workshopUserService;
+    private final AppUserRepository appUserRepository;
+    private final WorkshopUserRepository workshopUserRepository;
+    private final OTPService otpService;
 
     @Override
     public Booking saveBookingDetails(Booking booking) {
@@ -37,28 +49,42 @@ public class BookingServiceImpl implements BookingService {
         return bookingRepository.findAll();
     }
 
-    public List<WorkshopUser> getAllOpenWorkshops() {
+    @Override
+    public void deleteBooking(String id) {
+        if (!bookingRepository.existsById(id)) {
+            throw new RuntimeException("Booking not found with id: " + id);
+        }
+        bookingRepository.deleteById(id);
+    }
+
+    public List<WorkshopStatusResponse> getAllOpenWorkshops() {
         List<WorkshopUser> allWorkShops = workshopUserService.getAllWorkshopUser();
-        List<WorkshopUser> openWorkShops = new ArrayList<>();
+        List<WorkshopStatusResponse> openWorkShops = new ArrayList<>();
         for(WorkshopUser workshopUser : allWorkShops) {
             if(workshopUser.getCurrentWorkshopStatus() == WorkshopStatus.OPEN) {
-                openWorkShops.add(workshopUser);
+                openWorkShops.add(new WorkshopStatusResponse(workshopUser.getId(), workshopUser.getName(),
+                        workshopUser.getUsername(),workshopUser.getCurrentWorkshopStatus(), workshopUser.getWorkshopAddress()));
             }
         }
         return openWorkShops;
     }
 
-    public String requestBooking(String appUserId, String workShopUserId,
+    public String requestBooking(String workShopUserId,
                                double amount, String vehicleDetails) {
 
-        // 1. Change the logic here to track the current logged-in user without explicitly searching
-        AppUser currentAppUser = appUserService.getAppUserById(appUserId);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        AppUser currentAppUser = appUserService.getAppUserByUsername(username);
 
         // 2. Throw exception if workshopUser doesn't exist and catch it in the controller layer
         // and return relevant response.
         WorkshopUser workshopUser = workshopUserService.getWorkshopUserById(workShopUserId);
-
-        Booking booking = bookingRepository.save(new Booking(amount, vehicleDetails));
+        if(workshopUser == null) {
+            throw new RuntimeException("The requested workShop was not found! Id = "+workShopUserId);
+        } else if(workshopUser.getCurrentWorkshopStatus() != WorkshopStatus.OPEN) {
+            throw new RuntimeException("The requested workShop is closed now! Id = "+workShopUserId);
+        }
+        Booking booking = bookingRepository.save(new Booking(amount, vehicleDetails, currentAppUser.getId()));
 
         currentAppUser.getBookingHistory().add(booking);
         workshopUser.getBookingHistory().add(booking);
@@ -69,25 +95,180 @@ public class BookingServiceImpl implements BookingService {
         return booking.getId();
     }
 
-    public Booking checkBookingStatus(String id) {
-        return getBookingById(id);
+    public BookingResponse checkBookingStatus(String id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        AppUser currentAppUser = appUserService.getAppUserByUsername(username);
+
+        if(!currentAppUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(id))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+id);
+        }
+
+        Booking currentBooking = getBookingById(id);
+        if(currentBooking == null) {
+            throw new RuntimeException("Booking id not found, id = "+id);
+        }
+
+        if(currentBooking.getCurrentStatus() == BookingStatus.STARTED)
+            return new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                null, null, null);
+
+        else
+            return new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                    currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                    currentBooking.getWorkshopUserId(), currentBooking.getWorkShopName(), currentBooking.getWorkShopAddress());
     }
 
-    public List<Booking> getStartedBookings(String workshopUserId) {
-        WorkshopUser workshopUser = workshopUserService.getWorkshopUserById(workshopUserId);
-        List<Booking> allBookings = workshopUser.getBookingHistory();
-        List<Booking> startedBookings = new ArrayList<>();
-        for(Booking booking : allBookings) {
-            if(booking.getCurrentStatus() == BookingStatus.STARTED) {
-                startedBookings.add(booking);
+    public List<BookingResponse> getStartedBookings() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        WorkshopUser currentWorkShopUser = workshopUserRepository.findByUsername(username)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        List<Booking> allBookings = currentWorkShopUser.getBookingHistory();
+        List<BookingResponse> startedBookings = new ArrayList<>();
+        for(Booking currentBooking : allBookings) {
+            if(currentBooking.getCurrentStatus() == BookingStatus.STARTED) {
+                startedBookings.add(new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                        currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                        null, null, null));
             }
         }
         return startedBookings;
     }
 
-    public Booking acceptBooking(String bookingId) {
-        Booking booking = getBookingById(bookingId);
-        booking.setCurrentStatus(BookingStatus.BOOKED);
-        return saveBookingDetails(booking);
+    public BookingResponse acceptBooking(String bookingId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        WorkshopUser currentWorkShopUser = workshopUserRepository.findByUsername(username)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        if(!currentWorkShopUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(bookingId))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+bookingId);
+        }
+
+        Booking currentBooking = getBookingById(bookingId);
+        currentBooking.setCurrentStatus(BookingStatus.BOOKED);
+        currentBooking.setWorkshopUserId(currentWorkShopUser.getId());
+        currentBooking.setWorkShopName(currentWorkShopUser.getName());
+        currentBooking.setWorkShopAddress(currentWorkShopUser.getWorkshopAddress());
+
+        saveBookingDetails(currentBooking);
+
+        return new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                currentBooking.getWorkshopUserId(), currentWorkShopUser.getName(), currentWorkShopUser.getWorkshopAddress());
+    }
+
+    public BookingResponse rejectBooking(String bookingId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        WorkshopUser currentWorkShopUser = workshopUserRepository.findByUsername(username)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        if(!currentWorkShopUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(bookingId))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+bookingId);
+        }
+
+        Booking currentBooking = getBookingById(bookingId);
+        // Temporary Logic implemented for now
+        // In reality after booking is rejected, user should get prompt to request booking in another workshop
+        if(currentBooking.getCurrentStatus() != BookingStatus.BOOKED)
+            throw new RuntimeException("Booking id provided cannot be set REJECTED, id = "+bookingId);
+
+        currentBooking.setCurrentStatus(BookingStatus.REJECTED);
+        currentBooking.setWorkshopUserId(currentWorkShopUser.getId());
+
+        saveBookingDetails(currentBooking);
+
+        return new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                currentBooking.getWorkshopUserId(), currentWorkShopUser.getName(), currentWorkShopUser.getWorkshopAddress());
+    }
+
+    public BookingResponse startJourney(String bookingId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        AppUser currentAppUser = appUserService.getAppUserByUsername(username);
+
+        if(!currentAppUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(bookingId))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+bookingId);
+        }
+
+        Booking currentBooking = getBookingById(bookingId);
+        if(currentBooking.getCurrentStatus() != BookingStatus.BOOKED)
+            throw new RuntimeException("Booking id provided cannot be set ON_THE_WAY, id = "+bookingId);
+        currentBooking.setCurrentStatus(BookingStatus.ON_THE_WAY);
+
+        saveBookingDetails(currentBooking);
+
+        return new BookingResponse(currentBooking.getId(), currentBooking.getAmount(), currentBooking.getVehicleDetails(),
+                currentBooking.getCurrentStatus(), currentBooking.getBookingStartedTime(), currentBooking.getBookingCompletedTime(),
+                currentBooking.getWorkshopUserId(), currentBooking.getWorkShopName(), currentBooking.getWorkShopAddress());
+    }
+
+    public BookingStatusResponse generateBookingOtp(String bookingId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        AppUser currentAppUser = appUserService.getAppUserByUsername(username);
+
+        if(!currentAppUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(bookingId))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+bookingId);
+        }
+
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new RuntimeException("Booking not found"));
+
+        String otp = otpService.generateOtp();
+        LocalDateTime expiry = otpService.getExpiryTime();
+
+        booking.setOtp(otp);
+        booking.setOtpExpiry(expiry);
+
+        bookingRepository.save(booking);
+        return new BookingStatusResponse(bookingId, booking.getCurrentStatus(), otp);
+    }
+
+    public void verifyOtpAndSetStatus(BookingRequestOtp bookingRequestOtp, BookingStatus bookingStatus) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        WorkshopUser currentWorkShopUser = workshopUserRepository.findByUsername(username)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+
+        if(!currentWorkShopUser.getBookingHistory()
+                .stream()
+                .anyMatch(b -> b.getId().equals(bookingRequestOtp.getId()))) {
+            throw new RuntimeException("Booking id provided is not current user's booking, id = "+bookingRequestOtp.getId());
+        }
+
+        Booking booking = bookingRepository.findById(bookingRequestOtp.getId())
+                .orElseThrow(() -> new RuntimeException("Booking not found"));
+
+        if (otpService.isOtpExpired(booking.getOtpExpiry())) {
+            throw new RuntimeException("OTP expired. Please regenerate.");
+        }
+
+        if (!booking.getOtp().equals(bookingRequestOtp.getOtp())) {
+            throw new RuntimeException("Invalid OTP.");
+        }
+
+        booking.setCurrentStatus(bookingStatus);
+        booking.setOtp(null);
+
+        if(bookingStatus == BookingStatus.COMPLETED)
+            booking.setBookingCompletedTime(LocalDateTime.now());
+
+        bookingRepository.save(booking);
     }
 }
