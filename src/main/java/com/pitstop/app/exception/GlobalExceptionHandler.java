72b303ba48e441ff28eb175ac14c36556a5f3bea
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2025-10-24 22:32:23 +0530

    Update Global Exception handler.

--- src/main/java/com/pitstop/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pitstop/app/exception/GlobalExceptionHandler.java
@@ -17,9 +17,9 @@ public class GlobalExceptionHandler {
         return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND, request.getDescription(false));
     }
 
-    @ExceptionHandler(EmailAlreadyExistException.class)
-    public ResponseEntity<?> handleEmailAlreadyExist(EmailAlreadyExistException ex , WebRequest request){
-        return buildErrorResponse(ex.getMessage(),HttpStatus.BAD_REQUEST,request.getDescription(false));
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<?> handleUserAlreadyExist(UserAlreadyExistException ex , WebRequest request){
+        return buildErrorResponse(ex.getMessage(),HttpStatus.CONFLICT,request.getDescription(false));
     }
     private ResponseEntity<Map<String, Object>> buildErrorResponse(String message, HttpStatus status, String path) {
         Map<String, Object> errorBody = new HashMap<>();
