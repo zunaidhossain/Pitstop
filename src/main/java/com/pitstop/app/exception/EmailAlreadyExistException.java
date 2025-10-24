e8c4e6d0d17eb64618cc2007ffef114b4e360543
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2025-10-24 22:33:58 +0530

    Removed unused file.

--- src/main/java/com/pitstop/app/exception/EmailAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.pitstop.app.exception;
-
-public class EmailAlreadyExistException extends RuntimeException{
-    public EmailAlreadyExistException(String message){
-        super(message);
-    }
-}
