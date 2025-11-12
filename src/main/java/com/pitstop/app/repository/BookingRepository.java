cf43503bea60b8e708ed9d4a98c26bd9ae9a0c59
Zunaid Hossain <zunaidhossain.india@gmail.com>
2025-11-12 20:52:47 +0530

    Added method to fetch bookingHistory

--- src/main/java/com/pitstop/app/repository/BookingRepository.java
+++ src/main/java/com/pitstop/app/repository/BookingRepository.java
@@ -3,5 +3,9 @@ package com.pitstop.app.repository;
 import com.pitstop.app.model.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends MongoRepository<Booking, String> {
+    List<Booking> findByAppUserIdOrderByBookingStartedTimeDesc(String appUserId);
+    List<Booking> findByWorkshopUserIdOrderByBookingStartedTimeDesc(String workshopUserId);
 }
