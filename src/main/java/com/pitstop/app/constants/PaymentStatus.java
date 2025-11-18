2efc52901b45347f053be1956ba12f1c13c0c246
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2025-11-18 21:30:37 +0530

    ENUM for PaymentStatus

---
+++ src/main/java/com/pitstop/app/constants/PaymentStatus.java
@@ -0,0 +1,12 @@
+package com.pitstop.app.constants;
+
+public enum PaymentStatus {
+    INITIATED,
+    ORDER_CREATED,
+    SUCCESS,
+    FAILED,
+    REFUNDED,
+    EXPIRED,
+    PAID,
+    NOT_PAID,
+}
