7a5f7afe91b9f04caa3dc627552d62ffdb41bf54
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2025-11-30 02:20:02 +0530

    DTO for workshop service request

---
+++ src/main/java/com/pitstop/app/dto/WorkshopServiceTypeRequest.java
@@ -0,0 +1,10 @@
+package com.pitstop.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WorkshopServiceTypeRequest {
+    private String workshopServiceType;
+}
