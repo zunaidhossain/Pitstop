275a3410ce3d980e842436ce91a8ad273cdd66e5
Santojeet Chakraborty <santojeetchakraborty@gmail.com>
2025-11-30 02:16:31 +0530

    ENUM for Workshop Service Type

---
+++ src/main/java/com/pitstop/app/constants/WorkshopServiceType.java
@@ -0,0 +1,11 @@
+package com.pitstop.app.constants;
+
+public enum WorkshopServiceType {
+    ENGINE_REPAIR,
+    OIL_CHANGE,
+    BATTERY_REPLACEMENT,
+    TYRE_REPLACEMENT,
+    BRAKE_SERVICE,
+    AC_REPAIR,
+    GENERAL_SERVICE
+}
